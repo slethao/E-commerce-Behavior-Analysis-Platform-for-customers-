2edb1def5a5c2ebae96866ef7824ca1b755c68e2
Somer Le <thaosle4@gmail.com>
2025-09-01 15:57:53 -0400

    creating the files for each step of the project (might make more)

---
+++ Python Scripts/Cleaning_data.py
@@ -0,0 +1,12 @@
+import kagglehub
+
+path = kagglehub.dataset_download("mehmetisik/amazon-review")
+print(f"Path to dataset files: {path}")
+# class FeedBackAPI():
+#     def __int__(self):
+#         self._path = kaggle_json
+#         self.username = user_username
+#         self._key = user_key
+
+#     def get_http_request(self):
+#         pass
