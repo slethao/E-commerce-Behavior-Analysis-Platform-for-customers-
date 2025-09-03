89b0001ccbd69dc5bd139f1f76cffc70787519a1
Somer Le <thaosle4@gmail.com>
2025-09-03 18:44:21 -0400

    needed to reshift the solutions but it works offically

--- Python Scripts/order_data.py
+++ Python Scripts/order_data.py
@@ -33,16 +33,22 @@ class Order_Data():
             alter_line_02 = self.parse_and_remove_groups_front("asin",splited_ls[0], alter_line)
             alter_line_03 = self.parse_and_remove_groups_front("reviewerName",splited_ls[0],alter_line_02)
 
-            alter_line_04 = self.parse_and_remove_groups_end("reviewTime",splited_ls[len(splited_ls)-1], alter_line_03)
-            alter_line_05 = self.parse_and_remove_groups_end("day_diff",splited_ls[len(splited_ls)-1], alter_line_04)
-            alter_line_06 = self.parse_and_remove_groups_end("helpful_yes",splited_ls[len(splited_ls)-1], alter_line_05)
-            alter_line_07 = self.parse_and_remove_groups_end("total_vote",splited_ls[len(splited_ls)-1], alter_line_06)
-            overall = "".join([element for element in alter_line_07 if re.search("^\d\.\d$", element)])
-            index_counter = [index for index in range(len(alter_line_07)) if overall == splited_ls[index]]
-            self._group_map["overall"].append(index_counter[0])
+            alter_line_04 = self.parse_and_remove_groups_end("total_vote",splited_ls[len(splited_ls)-1], alter_line_03)
+            alter_line_05 = self.parse_and_remove_groups_end("helpful_yes",splited_ls[len(splited_ls)-1], alter_line_04)
+            alter_line_06 = self.parse_and_remove_groups_end("day_diff",splited_ls[len(splited_ls)-1], alter_line_05)
+            alter_line_07 = self.parse_and_remove_groups_end("reviewTime",splited_ls[len(splited_ls)-1], alter_line_06)# reviewTime
+            alter_line_08 = self.parse_and_remove_groups_end("unixReviewTime",splited_ls[len(splited_ls)-1], alter_line_07)
+            self._group_map["helpful"].append(f"{alter_line_08[0]},{alter_line_08[1]}")
+            alter_line_08.remove(alter_line_08[0])
+            alter_line_08.remove(alter_line_08[0])
+        
+            overall = "".join([element for element in alter_line_08 if re.search("^\d\.\d$", element)])
+            index_counter = [index for index in range(len(alter_line_08)) if overall == splited_ls[index]]
+            self._group_map["overall"].append(overall)
             self._group_map["reviewText"].append("".join(splited_ls[0 :index_counter[0]:]))
             self._group_map["summary"].append("".join(splited_ls[index_counter[0]+1:len(splited_ls):]))
 
+###################################################################
             # splited_ls = line.split(",")
             # self._group_map["reviewerID"].append(splited_ls[0])
             # self._group_map["asin"].append(splited_ls[1])
