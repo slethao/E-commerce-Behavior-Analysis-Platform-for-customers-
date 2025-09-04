367fbbfdacf193f9e23d4da89aef465999fde69d
Somer Le <thaosle4@gmail.com>
2025-09-04 10:18:32 -0400

    altered the main program with the Collection Layer

--- Python Scripts/Analysis_Made.py
+++ Python Scripts/Analysis_Made.py
@@ -1,8 +1,14 @@
 import Collection_Layer as cl
+import re
 
 def main():
     file_path = "Batch Ingestion/Ingestion Layer/amazon_review.csv"
-    print(cl.Collection_Layer(file_path).verify_data())
+    with open(file_path, 'r') as data:
+        all_groups = data.readline().split(",")
+        content = data.readlines()
+        collection = cl.Collection_Layer(content)
+        print(collection.map_groups()["summary"]) # helpful was suppose to be day_diff
+
     print("this works ^-^")
 
 main()
