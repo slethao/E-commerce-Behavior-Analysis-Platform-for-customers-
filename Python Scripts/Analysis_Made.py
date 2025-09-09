6670e085b8bdd1a18c8387ad9a08f33a526dc997
Somer Le <thaosle4@gmail.com>
2025-09-09 11:10:40 -0400

    refixed the mechnaics of the proccessing layer in the main program

--- Python Scripts/Analysis_Made.py
+++ Python Scripts/Analysis_Made.py
@@ -1,5 +1,6 @@
 import Collection_Layer as cl
 import Processing_Layer as pl
+import Storage_Layer as sl
 
 def record_results(file_path, results):
     with open(file_path, "w") as content:
@@ -12,15 +13,17 @@ def main():
         content = data.readlines()
         collection = cl.Collection_Layer(content)
         groups_sorted = collection.map_groups() #NOTE use the data here for the next layer (proccessing)
-        
-
         file_path_one = "Batch Ingestion/Processing Layer/processed_data.csv" # this is for the processed data after the word filter
         file_path_two = "Batch Ingestion/Processing Layer/sentiment_analysis_review.csv"
         file_path_three = "Batch Ingestion/Processing Layer/overall_mean_review.csv"
         file_path_four = "Batch Ingestion/Collection Layer/sorted_collection.csv"
+        file_list = [file_path_one, file_path_two, file_path_three, file_path_four]
+        storage = sl.Storage_Layer(file_path_one, all_groups) #
         process_collect = pl.Processing_layer(groups_sorted)
+        
         all_sorted_csv_format = [process_collect.get_data(i) for i in range(len(groups_sorted["reviewerID"]))]
-        collection_recorded = [",".join(process_collect.get_data(i)) for i in range(len(groups_sorted["reviewerID"]))]
+        csv_format_list_of_lines = [",".join(process_collect.get_data(i)) for i in range(len(groups_sorted["reviewerID"]))]
+
         if collection.verify_schema() == True and len([group for group in all_groups if collection.verify_datatype_group(group) == True]) == 12:
             for i in range(len(all_sorted_csv_format)):
                 line = all_sorted_csv_format[i]
@@ -28,10 +31,23 @@ def main():
                     process_collect.remove_record(i)
             altered_review_content = process_collect.text_cleaning()
             process_collect.set_value(altered_review_content, "reviewText")
-            record_results(file_path_one, process_collect.get_csv_format())
-            record_results(file_path_two, process_collect.calculate_reviews(all_sorted_csv_format)) # senitment ^-^
-            record_results(file_path_three, process_collect.overall_review_product(all_sorted_csv_format)) # calcuation of the overall reviews
-            record_results(file_path_four, collection_recorded)
+            
+            proccess_result = [process_collect.get_csv_format(),
+                               process_collect.calculate_reviews(all_sorted_csv_format),
+                               process_collect.overall_review_product(all_sorted_csv_format),
+                               csv_format_list_of_lines]
+            #NOTE for loop start
+            file_name_list = ["Processed Data", "Sentiment Analysis", "Overall Mean", "Sorted Collection"]
+            # connection
+            for i in range(len(file_list)):
+                storage.set_table_name(file_name_list[i])
+                storage.set_file_path(file_list[i])
+                print(proccess_result[i])
+                print("****")
+                record_results(file_list[i], proccess_result[i])
+                #storage.convert_csv_to_table()
+
+            #NOTE for loop end
         else:
             print("This data is unfit for consluding anything...")
     print("this works ^-^")
