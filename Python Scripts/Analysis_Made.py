ae00f5979c39899a69c219aa29b07c828d83259d
Somer Le <thaosle4@gmail.com>
2025-09-14 15:31:23 -0400

    adding the code for streamlining the workflow

--- Python Scripts/Analysis_Made.py
+++ Python Scripts/Analysis_Made.py
@@ -1,77 +1,20 @@
 import Prefect_DAG_helper_func as automated_dag
+from kivy.app import App
+import Main_Window
 
+class MainApplication(App):
+    def __init__(self):
+        pass
+
+    def build_application(self):
+        self._title = ""
+        # return MainWindow() 
 def main():
     file_path = "Batch Ingestion/Ingestion Layer/amazon_review.csv"
     with open(file_path, 'r') as data:
         all_groups = data.readline().split(",")
         content = data.readlines()
         automated_dag.create_the_dag(content, file_path, all_groups)
-
-
-    print("this works ^-^")
-    # file_path = "Batch Ingestion/Ingestion Layer/amazon_review.csv"
-    # table_name = "processed_data"
-    # with open(file_path, 'r') as data:
-    #     all_groups = data.readline().split(",")
-    #     content = data.readlines()
-    #     automate_workflow = automated_dag.Prefect_DAG(content, file_path, all_groups)
-    #     automate_workflow.create_the_dag()
-
-    #     content = automate_workflow.pull_from_db(table_name)
-    #     print(f"Records for table: {len(content)} records.")
-
-
-
-
-
-
-
-        # #NOTE this is the first to go
-        # collection = cl.Collection_Layer(content)
-        # groups_sorted = collection.map_groups() #NOTE use the data here for the next layer (proccessing)
-        # #NOTE this the end of the first ot go
-        # file_path_four = "Batch Ingestion/Collection Layer/sorted_collection.csv"
-        # file_path_one = "Temp_Folder/temp_amazon_reviews_02.csv"
-
-        # storage = sl.Storage_Layer(file_path_one, all_groups) #
-        # query_obj = ql.Query_Layer()
-        # transform_obj = dt.Transforming_Task(file_path_four)
-
-        # #NOTE this is teh second to go
-        # if collection.verify_schema() == True and len([group for group in all_groups if collection.verify_datatype_group(group) == True]) == 12:
-        # #NOTE this is the first to go
-            
-        #     #NOTE this will stay
-        #     transform_obj.create_temp_file() #NOTE temp file is created
-        #     #NOTE this will be the end of what will stay
-
-        #     #NOTE this will the get rid of
-        #     unique_records = transform_obj.missing_data(groups_sorted, "Temp_Folder/temp_amazon_reviews_02.csv")
-        #     mapped_keys = transform_obj.creating_map_keys(all_groups)
-        #     mapped_content = transform_obj.mapped_group(mapped_keys, unique_records)
-        #     tokenize_records = transform_obj.tokenize_data(mapped_content)
-        #     process_collect = pl.Processing_layer(mapped_content)
-        #     process_collect.set_value(tokenize_records, "reviewText")
-        #     saved_results = process_collect.get_csv_format()
-        #     transform_obj.save_dataset(file_path_one, saved_results) 
-        #     #NOTE this wil be the end of the get rid of
-
-        #     #NOTE this will be get rid of
-        #     storage.create_table_overall("processed_data", all_groups) #NOTE processed_data.csv
-        #     if storage.verify_table_filled("processed_data") == False:
-        #         storage.load_table_data("processed_data",
-        #                                 file_path_one,
-        #                                 all_groups) # tokenized data, text filtered and cleaned data
-        #         #print(f"These are the tables in postgres(proccessed_data): {query_obj.view_table_content("processed_data")}") # work
-        #         print(f"The number of records(proccessed_data): {len(query_obj.view_table_content("processed_data"))}")
-        #     else:
-        #         #print(f"These are the tables in postgres(proccessed_data): {query_obj.view_table_content("processed_data")}") # work
-        #         print(f"The number of records(proccessed_data): {len(query_obj.view_table_content("processed_data"))}")
-        #     #NOTE this will be the end of the get rid of
-
-        #     #NOTE now you you are done with the tranforming task now you have to do  Database Loading Task to finsih the DAG
-    
-    # 14 - 54
     print("this works ^-^")
 
 main()
