33a0d8896535f9eb848eba462692ee95f8628eb7
Somer Le <thaosle4@gmail.com>
2025-09-14 16:50:01 -0400

    edit the Query_Layer so when the data is pulled it gets reformatted so it is easier to filter data

--- Python Scripts/Query_Layer.py
+++ Python Scripts/Query_Layer.py
@@ -15,9 +15,20 @@ class Query_Layer():
                                             port="5432")
         self._cursor = self._connection.cursor()
     
-    def view_table_content(self, table_name: str) -> list:
+    def view_table_content(self, table_name: str) -> list[tuple]:
         try:
             self._cursor.execute(f'SELECT * FROM {table_name};')
             return self._cursor.fetchall()
         except psycopg2.Error as e:
             return e.pgerror
+        
+    def format_to_csv(self, table_content):
+        formatted_content = []
+        record_line = ""
+        for line in table_content:
+            record = line[1:len(line):]
+            for item in record:
+                record_line += f"{item}," 
+            formatted_content.append(f"{record_line[:len(record_line)-1:]}\n")
+            record_line  = ""
+        return formatted_content
