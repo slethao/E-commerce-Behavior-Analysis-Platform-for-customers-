ea8f760b76f4c6260b92519fb7f384dfd3be1df3
Somer Le <thaosle4@gmail.com>
2025-09-17 19:00:26 -0400

    working main programming for the NLM

--- Python Scripts/Analysis_Made.py
+++ Python Scripts/Analysis_Made.py
@@ -1,25 +1,19 @@
 import Prefect_DAG_helper_func as automated_dag
-from kivy.app import App
-import Medallion_Model as mm
+import Sentiment_Analysis_NLM as sal
 
-# class MainApplication(App):
-#     def __init__(self):
-#         pass
-
-    # def build_application(self):
-    #     self._title = ""
-    #     return MainWindow() 
 def main():
     file_path = "Batch Ingestion/Ingestion Layer/amazon_review.csv"
     with open(file_path, 'r') as data:
         all_groups = data.readline().split(",")
         content = data.readlines()
         table_name = "processed_data"
-        # automated_dag.create_the_dag(content, file_path, all_groups)
-        # print("this works ^-^")
-        medallion = mm.Medallion_Model(table_name)
-        raw_content = medallion.get_bronze()
-        print(medallion.transfer_to_silver(raw_content))
-        # print(medallion.transfer_to_gold())
+        completed_filtered = automated_dag.create_the_dag(content, file_path, all_groups)
+        mapped_out = automated_dag.collection_task(completed_filtered, all_groups)
+        ####################
+        transform = sal.Sentiment_Analysis_NLM(mapped_out)
+        transform.shuffle_data()
+        transform.train_model()
+        print(transform.evaluate_model())
+        print("this works ^-^ =^._.^=")
 
 main()
