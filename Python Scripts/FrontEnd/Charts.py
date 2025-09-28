231ba8014b8be5b99419dbcea6f8c6cc578f7214
Somer Le <thaosle4@gmail.com>
2025-09-28 10:46:18 -0400

    adding the documentation to the charts class

--- Python Scripts/FrontEnd/Charts.py
+++ Python Scripts/FrontEnd/Charts.py
@@ -1,12 +1,40 @@
 from nicegui import ui
 
+"""
+summary fo the class
+"""
 class Charts:
-    def __init__(self, positive_color, neutral_color, negative_color):
+    __slots__ = ('positive_color', 'neutral_color', 'negative_color')
+
+    def __init__(self, positive_color: str, neutral_color: str, negative_color: str):
+        """
+        Constructor for the class 'Charts'
+
+        Parameters:
+            positive_color (str): the hexacode that will represent the positive reviews in the dashboard 
+            neutral_color (str): the hexacode that will represent the neutral reviews in the dashboard
+            negative_color (str): the hexacode that will represent the negative reviews in the dashboard
+        """
         self._positive_color = positive_color
         self._neutral_color = neutral_color
         self._negative_color = negative_color
 
-    def make_highlight_charts_1(self, background, line_color, given_color, all_senti_reviews):
+    def make_highlight_charts_1(self, background: str, line_color: str, given_color: str, all_senti_reviews: list[str]) -> None:
+        """
+        This method creates a highchart that displays an 
+        interactive chart for the five-star reviews
+        (this returns nothing)
+
+        Parameters:
+            background (str): the hexacodethat will represent the color of the background
+            line_color (str): the hexacode will represent the color for the line colors for the grid lines and description text
+            given_color (str): the hexacode that will be used for the axis
+            all_senti_reivews (list[str]): a list that will have the sentimental positive, neutral, and negative of the
+                                            5-star reviews
+                                                index 0: is the positive
+                                                index 1: is the neutral
+                                                index 2: is the negative 
+        """
         ui.highchart({
             'title': False,
             'colors': [self._positive_color, self._neutral_color, self._negative_color],
@@ -34,7 +62,23 @@ class Charts:
             }
         }).classes(f'w-full h-90 [&_g.highcharts-legend-item_text]:!fill-[{line_color}] mt-[10px] [&_path.highcharts-grid-line]:!stroke-[{line_color}]')
     
-    def make_highlight_charts(self, background, line_color, given_color, senti_comments):
+    def make_highlight_charts(self, background: str, line_color: str, given_color: str, senti_comments: list[float]) -> None:
+        """
+        This method creates a highchart that displays an 
+        interactive chart for the sentiment analysi on the
+        review comments
+        (this returns nothing)
+
+        Parameters:
+            background (str): the hexacodethat will represent the color of the background
+            line_color (str): the hexacode will represent the color for the line colors for the grid lines and description text
+            given_color (str): the hexacode that will be used for the axis
+            senti_comments (list[float]): a list that will have the sentimental positive, neutral, and negative of the
+                                            review comments
+                                                index 0: is the positive
+                                                index 1: is the neutral
+                                                index 2: is the negative 
+        """
         ui.highchart({
             'title': False,
             'colors': [self._positive_color, self._neutral_color, self._negative_color],
