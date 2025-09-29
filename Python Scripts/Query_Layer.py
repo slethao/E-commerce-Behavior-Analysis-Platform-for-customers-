da4eceda7c3f5851f8ff3f870e6f6ec12a82b76c
Somer Le <thaosle4@gmail.com>
2025-09-29 12:24:04 -0400

    adding comments to the class quersy layer

--- Python Scripts/Query_Layer.py
+++ Python Scripts/Query_Layer.py
@@ -3,8 +3,25 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+"""
+The class is used to represent
+the Query Layer in the Batch Ingestion
+"""
 class Query_Layer():
     def __init__(self):
+        """
+        The Constructor 'Query Layer'
+            Parameter:
+                None
+            Private Members:
+                user_name = the username to access the database
+                password = the password to access teh database
+                database_name = the data base name
+                connection = the connection infomration to access
+                                the database
+                cursor = the cursor that allows you to edit objects
+                            such as tables into the database
+        """
         super().__init__
         self._user_name = os.getenv("DB_USER_NAME")
         self._password = os.getenv("PASSWORD")
@@ -16,6 +33,14 @@ class Query_Layer():
         self._cursor = self._connection.cursor()
     
     def view_table_content(self, table_name: str) -> list[tuple]:
+        """
+        The method use to look at the content in table that exisit
+        in the database
+            Parameter:
+                table_name (str): the name of the program wants to view
+            Return:
+                all the content in the table
+        """
         try:
             self._cursor.execute(f'SELECT * FROM {table_name};')
             return self._cursor.fetchall()
@@ -23,6 +48,16 @@ class Query_Layer():
             return e.pgerror
         
     def format_to_csv(self, table_content: list[tuple])-> list[str]:
+        """
+        The method is used to format all the conetent sorted into a map
+        and turn it into a list of reocords with its respectively ordered
+        groups
+            Parameter:
+                table_content (list[tuple]): all the content tha was pulled
+                                                from the table
+            Return:
+                a list of records that were created from the mapped content
+        """
         formatted_content = []
         record_line = ""
         for line in table_content:
